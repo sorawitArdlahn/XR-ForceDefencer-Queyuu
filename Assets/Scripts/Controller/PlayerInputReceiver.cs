f454e4edd7b792ac663ddf700c095dff448bb651
sorawitArdlahn <98576891+sorawitArdlahn@users.noreply.github.com>
2025-01-08 20:15:52 +0700

    Initial commit

---
+++ Assets/Scripts/Controller/PlayerInputReceiver.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Controller
+{
+    public class PlayerInputReceiver : MonoBehaviour
+    {
+        private Vector3 _movement;
+        private Vector2 _lookPosition;
+        private bool _isJumping;
+        private bool _isDashing;
+        private bool _isFlying;
+        private bool _isFire;
+        private bool _isSprinting;
+        private bool _isDriving = false;
+        
+        private void OnMovement(InputValue value)
+        {
+            _movement = value.Get<Vector3>();
+        }
+
+        private void OnJump(InputValue value)
+        {
+            _isJumping = value.isPressed;
+        }
+
+        private void OnLook(InputValue value)
+        {
+            _lookPosition = value.Get<Vector2>();
+        }
+
+        private void OnFly(InputValue value)
+        {
+            _isFlying = value.isPressed;
+        }
+        
+        private void OnDash(InputValue value)
+        {
+            _isDashing = value.isPressed;
+        }
+
+        private void OnFire(InputValue value)
+        {
+            _isFire = value.isPressed;
+        }
+
+        private void OnSprint(InputValue value)
+        {
+            _isSprinting = value.isPressed;
+        }
+
+        private void OnDrive(InputValue value)
+        {
+            _isDriving = !_isDriving;
+        }
+
+        public Vector3 Movement
+        {
+            get => _movement;
+            set => _movement = value;
+        }
+
+        public Vector2 LookPosition
+        {
+            get => _lookPosition;
+            set => _lookPosition = value;
+        }
+
+        public bool IsJumping
+        {
+            get => _isJumping;
+            set => _isJumping = value;
+        }
+
+        public bool IsDashing
+        {
+            get => _isDashing;
+            set => _isDashing = value;
+        }
+
+        public bool IsFlying
+        {
+            get => _isFlying;
+            set => _isFlying = value;
+        }
+
+        public bool IsFire
+        {
+            get => _isFire;
+            set => _isFire = value;
+        }
+
+        public bool IsSprinting
+        {
+            get => _isSprinting;
+            set => _isSprinting = value;
+        }
+
+        public bool IsDriving
+        {
+            get => _isDriving;
+            set => _isDriving = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Controller/PlayerMovementCtrl.cs
@@ -0,0 +1,251 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+namespace Controller
+{
+    public class PlayerMovementCtrl : MonoBehaviour
+    {
+        [Header("Input Receiver")] public PlayerInputReceiver playerInputReceiver;
+
+        [Header("Character Info")] public int fuel; //use fuel form robot info class instead
+        [Header("Settings")] 
+        public float gravity = 9.81f; // use for config gravity in game
+        private bool _isDragToGroundActive; // use for turn on-off DragToGround() 
+        private MovementState movementState;
+        private bool _isCanUseFuel; // for delay using fuel, solve frame looping using fuel so much in 1 second
+
+        [Header("Ground Check")] public float playerHeight;
+        public Transform groundCheck;
+        public LayerMask groundLayer;
+        private bool isGrounded;
+
+        [Header("Cockpit Movement")] public Transform cockpitTransform;
+        public float sensitivityX;
+        public float sensitivityY;
+        public float angleLimit = 90; // degree
+
+        private float lookX;
+        private float lookY;
+        private float cockpitRotationX;
+        private float cockpitRotationY;
+
+        [Header("Character Movement | Walk&Sprint")] [SerializeField]
+        private float moveSpeed = 0;
+        public float walkSpeed;
+        public float sprintSpeed;
+        public int sprintRequiredFuel; //only sprint 
+        
+        private float horizontalInput;
+        private float verticalInput;
+        private bool isInputSprinting;
+        private Vector3 moveDirection;
+        private Rigidbody rb;
+        public float groundDrag;
+
+        [Header("Character Movement | Dash")] 
+        public float dashForce;
+        public int dashRequiredFuel;
+        private bool isInputDashing;
+        
+        [Header("Character Movement | Jump")] 
+        public float jumpForce;
+        private bool isInputJumping;
+
+        [Header("Character Movement | Fly")] 
+        public float ascendSpeed;
+        public float airMultiplier;
+        public int flyRequiredFuel;
+        private bool isInputFlying;
+        
+
+        private void Start()
+        {
+            // make cursor drag in the middle of the screen and disappear 
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            rb = GetComponent<Rigidbody>();
+            rb.freezeRotation = true;
+
+            Physics.gravity = new Vector3(0, -this.gravity, 0);
+            _isDragToGroundActive = true;
+            _isCanUseFuel = true;
+        }
+
+        private void Update()
+        {
+            ReceiveInput();
+            GroundCheck();
+            MovementStateHandler();
+            SpeedControl();
+            DragCharacter();
+            MoveCockpit();
+        }
+
+        private void FixedUpdate()
+        {
+            if (isInputDashing && (movementState != MovementState.Sprinting) && CheckFuel(dashRequiredFuel)) Dash();
+            if (!isInputFlying && !isInputJumping && !isGrounded) DragToGround();
+            if (isInputJumping && isGrounded) Jump();
+            if (isInputFlying && !isGrounded && CheckFuel(flyRequiredFuel)) Fly();
+            MoveCharacter();
+            
+        }
+
+        private void ReceiveInput()
+        {
+            // get variable from PlayerInputReceiver
+            lookX = playerInputReceiver.LookPosition.x * Time.deltaTime * sensitivityX;
+            lookY = playerInputReceiver.LookPosition.y * Time.deltaTime * sensitivityY;
+
+            horizontalInput = playerInputReceiver.Movement.x;
+            verticalInput = playerInputReceiver.Movement.z;
+            
+            isInputJumping = playerInputReceiver.IsJumping;
+            isInputFlying = playerInputReceiver.IsFlying;
+            isInputSprinting = playerInputReceiver.IsSprinting;
+            isInputDashing = playerInputReceiver.IsDashing;
+        }
+
+        private void GroundCheck()
+        {
+            //isGrounded = Physics.Raycast(groundCheck.position, Vector3.down,playerHeight * 0.5f + 0.5f, groundLayer);
+            isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, groundLayer);
+        }
+
+        private void MovementStateHandler()
+        {
+            // Mode - Sprinting
+            if (isGrounded && isInputSprinting && CheckFuel(sprintRequiredFuel))
+            {
+                movementState = MovementState.Sprinting;
+                moveSpeed = sprintSpeed;
+                StartCoroutine(UseFuel(sprintRequiredFuel));
+            }
+
+            // Mode - Walking
+            else if (isGrounded)
+            {
+                movementState = MovementState.Walking;
+                moveSpeed = walkSpeed;
+                
+            }
+
+            // Mode - Air
+            else
+            {
+                movementState = MovementState.Air;
+                moveSpeed = walkSpeed * airMultiplier;
+                
+            }
+        }
+
+        private void DragCharacter()
+        {
+            if (isGrounded) rb.drag = groundDrag;
+            else rb.drag = 0;
+        }
+
+        private void MoveCockpit()
+        {
+            cockpitRotationY += lookX;
+            cockpitRotationX -= lookY;
+            // make cockpit have a limit of only 90 degrees of head tilt.
+            cockpitRotationX = Mathf.Clamp(cockpitRotationX, -angleLimit, angleLimit);
+
+            cockpitTransform.rotation = Quaternion.Euler(cockpitRotationX, cockpitRotationY, 0);
+            transform.rotation = Quaternion.Euler(0, cockpitRotationY, 0);
+        }
+
+        private void MoveCharacter()
+        {
+            //calculate movement direction
+            moveDirection = transform.forward * verticalInput + transform.right * horizontalInput;
+            rb.AddForce(moveDirection.normalized * (moveSpeed * 10f * rb.mass), ForceMode.Force);
+        }
+
+        private void SpeedControl()
+        {
+            Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+            Vector3 flyFlatVelocity = new Vector3(0, rb.velocity.y, 0);
+
+            // limit velocity of needed
+            if (flatVelocity.magnitude > moveSpeed)
+            {
+                Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+                rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+            }
+
+            if (flyFlatVelocity.magnitude > ascendSpeed)
+            {
+                Vector3 limitedVelocity = flyFlatVelocity.normalized * ascendSpeed;
+                rb.velocity = new Vector3(rb.velocity.x, limitedVelocity.y, rb.velocity.z);
+            }
+        }
+
+        private void Jump()
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+            rb.AddForce(transform.up * (jumpForce * rb.mass), ForceMode.Impulse);
+        }
+
+        private void Fly()
+        {
+            ; // not hold fly and character still stand on ground
+            StartCoroutine(UseFuel(flyRequiredFuel));
+            rb.AddForce(transform.up * (ascendSpeed * rb.mass), ForceMode.Impulse);
+        }
+
+        private void Dash()
+        {
+            StartCoroutine(UseFuel(dashRequiredFuel));
+            if (moveDirection == new Vector3(0,0,0))
+            {
+                moveDirection = transform.forward;
+            }
+            
+            rb.AddForce(moveDirection.normalized * (dashForce * rb.mass), ForceMode.Impulse);
+        }
+        
+        private void DragToGround()
+        {
+            if (!_isDragToGroundActive) return;
+            float tempVelocity = rb.velocity.y;
+            tempVelocity += -gravity;
+            rb.velocity = new Vector3(rb.velocity.x, tempVelocity * 1, rb.velocity.z);
+        }
+
+        private IEnumerator UseFuel(int fuelAmount)
+        {
+            if (_isCanUseFuel)
+            {
+                _isCanUseFuel = false;
+                fuel -= fuelAmount;
+                //place api use fuel here
+                yield return new WaitForSeconds(0.1f);
+                _isCanUseFuel = true;
+            }
+        }
+
+        private bool CheckFuel(int fuelAmount)
+        {
+            if (fuel >= fuelAmount)
+            {
+                return true;
+            }
+            
+            return false;
+        }
+    } 
+}
+public enum MovementState
+    {
+        Walking,
+        Sprinting,
+        Air,
+        AutoDrive,
+    }
+
