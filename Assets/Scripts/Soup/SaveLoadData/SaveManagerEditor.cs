32f982d962deded651d01de7a29fbfabb514b41d
Siraphop Cheepudomwit <siraphop.che@ku.th>
2025-02-26 14:13:13 +0700

    feat(Save) : fix method error in SaveManagerEditor class
    SaveManagerEditor class require GameData parameter inside, had none before

--- Assets/Scripts/Soup/SaveLoadData/SaveManagerEditor.cs
+++ Assets/Scripts/Soup/SaveLoadData/SaveManagerEditor.cs
@@ -1,19 +1,20 @@
+using GameController;
 using UnityEditor;
 using UnityEngine;
 
 namespace System.Persistence.Editor {
     [CustomEditor(typeof(SaveLoadSystem))]
     public class SaveManagerEditor : UnityEditor.Editor {
-        public override void OnInspectorGUI() {
+         public override void OnInspectorGUI() {
             base.OnInspectorGUI();
 
             SaveLoadSystem saveLoadSystem = (SaveLoadSystem) target;
-            string gameName = saveLoadSystem.gameData.Name;
+            string gameName = SaveLoadSystem.Instance.gameData.Name;
 
             DrawDefaultInspector();
 
             if (GUILayout.Button("Save Game")) {
-                saveLoadSystem.SaveGame();
+                saveLoadSystem.SaveGame(GameManager.Instance.currentGameData);
             }
 
             if (GUILayout.Button("Load Game")) {
