aeddfabadc90368c2bad2cc6042b24128f942abd
Siraphop Cheepudomwit <siraphop.che@ku.th>
2025-03-14 11:23:43 +0700

    fix(Level) : fix AI nav mesh to only use the collider and not mesh

--- Assets/Scripts/Controller/Level/MapGeneratorController.cs
+++ Assets/Scripts/Controller/Level/MapGeneratorController.cs
@@ -33,6 +33,8 @@ public class MapGeneratorController : MonoBehaviour
 
     private int backupUsedCount = 0;
 
+    protected GameObject gridParent;
+
     private Dictionary<string, int> tileTypeFrequencies = new Dictionary<string, int>();
     private Dictionary<string, float> tileTypeWeights = new Dictionary<string, float>();
 
@@ -42,17 +44,18 @@ public class MapGeneratorController : MonoBehaviour
 
     [SerializeField] private bool CreateWall;
 
-    GameObject gridParent;
+    protected float borderHeight;
 
-    float borderHeight;
+    private GameObject wallParent;
 
-    public bool IsInitialized { get; private set; }
 
     //[SerializeField]
+    //NavMeshSurfaceComponent
     private NavMeshSurface navMeshSurface;
 
     [Header("Events")]
     public GameEvent mapGenerated;
+    public bool IsInitialized { get; private set; }
 
     public Vector3 GetPlayerSpawnPosition() {
         foreach (CellV2 cell in gridComponent) {
@@ -76,14 +79,15 @@ public class MapGeneratorController : MonoBehaviour
     public void Awake() {
 
         gridComponent = new List<CellV2>();
+
         gridParent = new GameObject("GeneratedMap");
-        templateList = tileSet[UnityEngine.Random.Range(0, tileSet.Count)].mapPatternTemplatesInSet;
 
+        //Set NavMeshSurface
         navMeshSurface = gridParent.AddComponent<NavMeshSurface>();
+        navMeshSurface.useGeometry = UnityEngine.AI.NavMeshCollectGeometry.PhysicsColliders;
 
-        InitializeTileObjects();
-        InitializeTileTypeFrequencies();
-
+        //Set Wall Parent
+        wallParent = new GameObject("GeneratedWalls");
     }
 
     void InitializeGrid() {
@@ -122,34 +126,25 @@ public class MapGeneratorController : MonoBehaviour
     
     void CreateBorderWalls() {
     // Bottom border
-    GameObject bottomWall = Instantiate(wallPrefab, new Vector3((dimensions - 1) * cellV2Size / 2, borderHeight / 2, -1 * cellV2Size / 2), Quaternion.identity, gridParent.transform);
+    GameObject bottomWall = Instantiate(wallPrefab, new Vector3((dimensions - 1) * cellV2Size / 2, borderHeight / 2, -1 * cellV2Size / 2), Quaternion.identity, wallParent.transform);
     bottomWall.transform.localScale = new Vector3(dimensions * cellV2Size, borderHeight, bottomWall.transform.localScale.z);
 
     // Top border
-    GameObject topWall = Instantiate(wallPrefab, new Vector3((dimensions - 1) * cellV2Size / 2, borderHeight / 2, dimensions * cellV2Size - cellV2Size / 2), Quaternion.identity, gridParent.transform);
+    GameObject topWall = Instantiate(wallPrefab, new Vector3((dimensions - 1) * cellV2Size / 2, borderHeight / 2, dimensions * cellV2Size - cellV2Size / 2), Quaternion.identity, wallParent.transform);
     topWall.transform.localScale = new Vector3(dimensions * cellV2Size, borderHeight, topWall.transform.localScale.z);
 
     // Left border
-    GameObject leftWall = Instantiate(wallPrefab, new Vector3(-1 * cellV2Size / 2, borderHeight / 2, (dimensions - 1) * cellV2Size / 2), Quaternion.identity, gridParent.transform);
+    GameObject leftWall = Instantiate(wallPrefab, new Vector3(-1 * cellV2Size / 2, borderHeight / 2, (dimensions - 1) * cellV2Size / 2), Quaternion.identity, wallParent.transform);
     leftWall.transform.localScale = new Vector3(leftWall.transform.localScale.x, borderHeight, dimensions * cellV2Size);
 
     // Right border
-    GameObject rightWall = Instantiate(wallPrefab, new Vector3(dimensions * cellV2Size - cellV2Size / 2, borderHeight / 2, (dimensions - 1) * cellV2Size / 2), Quaternion.identity, gridParent.transform);
+    GameObject rightWall = Instantiate(wallPrefab, new Vector3(dimensions * cellV2Size - cellV2Size / 2, borderHeight / 2, (dimensions - 1) * cellV2Size / 2), Quaternion.identity, wallParent.transform);
     rightWall.transform.localScale = new Vector3(rightWall.transform.localScale.x, borderHeight, dimensions * cellV2Size);
 
     // Top cover
-    GameObject topCover = Instantiate(wallPrefab, new Vector3((dimensions - 1) * cellV2Size / 2, borderHeight, (dimensions - 1) * cellV2Size / 2), Quaternion.Euler(90, 0, 0), gridParent.transform);
+    GameObject topCover = Instantiate(wallPrefab, new Vector3((dimensions - 1) * cellV2Size / 2, borderHeight, (dimensions - 1) * cellV2Size / 2), Quaternion.Euler(90, 0, 0), wallParent.transform);
     topCover.transform.localScale = new Vector3(dimensions * cellV2Size, dimensions * cellV2Size, topCover.transform.localScale.z);
     }
-
-    /*
-    private void UpdateTiles() {
-        foreach (MapPattern tile in tileObjects)
-        {
-            tile.updateNeighbors();
-        }
-    }
-    */
 
     //check for item with the least varations
     IEnumerator CheckEntropy() {
@@ -500,9 +495,24 @@ public class MapGeneratorController : MonoBehaviour
 
     //Initialize the map
     public void ResetMap() {
+
+
         foreach (Transform child in gridParent.transform) {
             Destroy(child.gameObject);
         }
+
+        foreach (Transform child in wallParent.transform) {
+            Destroy(child.gameObject);
+        }
+
+        //Select MapSet Prefab
+        templateList = tileSet[UnityEngine.Random.Range(0, tileSet.Count)].mapPatternTemplatesInSet;
+
+        InitializeTileObjects();
+        InitializeTileTypeFrequencies();
+
+        navMeshSurface.RemoveData();
+
         gridComponent.Clear();
         iteration = 0;
         backupUsedCount = 0;
